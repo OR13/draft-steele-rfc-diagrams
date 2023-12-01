45e785737f3b6d2d3b5d9b2cf5203da94657be99
Orie Steele <orie@transmute.industries>
2023-12-01 16:08:47 -0600

    :rainbow::rocket:

--- postinstall.sh
+++ postinstall.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-echo "hello world"
-
-apk add --no-cache \
-      chromium \
-      nss \
-      freetype \
-      harfbuzz \
-      ca-certificates \
-      ttf-freefont
-
-# Tell Puppeteer to skip installing Chrome. We'll be using the installed package.
-export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
-
-
-# Add user so we don't need --no-sandbox.
-addgroup -S pptruser && adduser -S -G pptruser pptruser \
-    && mkdir -p /home/pptruser/Downloads /app \
-    && chown -R pptruser:pptruser /home/pptruser \
-    && chown -R pptruser:pptruser /app
+alias mmdoc="echo"
